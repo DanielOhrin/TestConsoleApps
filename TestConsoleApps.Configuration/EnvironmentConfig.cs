f13a88713ec9450cc8aa36589f19d51bf19245d2
Daniel Ohrin <xqiam@outlook.com>
2023-08-30 21:39:29 -0500

    Created UserChoices class

--- TestConsoleApps.Configuration/EnvironmentConfig.cs
+++ TestConsoleApps.Configuration/EnvironmentConfig.cs
@@ -3,6 +3,6 @@
     public class EnvironmentConfig
     {
         public static Environment Environment { get; private set; } = Environment.Development;
-        public static bool IsDebug { get; private set; } = true;
+        public static bool IsDebug { get; private set; } = false;
     }
 }
